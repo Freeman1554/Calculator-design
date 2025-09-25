0afb805405f3c01847c2c8b7ddae42f5a3c41328
Agu Michael <engrcharles33@gmail.com>
2025-09-25 04:31:34 +0100

    Create simple calculator

---
+++ app.js
@@ -0,0 +1,11 @@
+const colors = require('colors')
+const calculate = require('./calculator') 
+const path = require('path')
+
+let basename = path.basename(__filename)
+
+console.log(colors.green(calculate.Add()))
+console.log(colors.blue(calculate.subtract()))
+console.log(colors.bgMagenta(calculate.multiply()))
+console.log(colors.bgYellow(calculate.divide()))
+console.log(colors.red(basename))
---
+++ calculator.js
@@ -0,0 +1,22 @@
+function Add(){
+   return 8 + 5
+}
+
+function subtract(){
+    return 8 - 5
+}
+
+function multiply(){
+    return 8 * 5
+}
+
+function divide(){
+    return Math.floor (8 / 5)
+}
+
+module.exports = {Add, subtract, multiply, divide}
+
+// console.log(Add())
+// console.log(subtract())
+// console.log(multiply())
+// console.log(divide())
